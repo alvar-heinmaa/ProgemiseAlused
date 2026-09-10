9012c9d0f20e080c6207d15501d07d62e0d90543
alvar-heinmaa <heinmaalvar@gmail.com>
2026-09-10 10:52:28 +0300

    IfAndElseNumbrid

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //konsoolis sisetame numbri
+            //see number salvestatakse string andemetüüpi
+            //toimub kontroll, kes sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            {
+                Console.WriteLine("sisesta number");
+                string input = Console.ReadLine();
+                //kontrollime, kas sisestatud väärtus on number
+                if (int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("sisestasid numbri");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR.Numbrid ei sisestatud");
+                }
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
