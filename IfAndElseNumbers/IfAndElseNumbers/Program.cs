7e763e566efc366c5c2a1ec78a84facfaae5256e
alvar-heinmaa <heinmaalvar@gmail.com>
2026-09-10 10:59:29 +0300

    IfAndElseNumbers

--- IfAndElseNumbers/IfAndElseNumbers/Program.cs
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -4,63 +4,27 @@
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
 
             //konsoolis sisetame numbri
             //see number salvestatakse string andemetüüpi
             //toimub kontroll, kes sisestatud väärtus on number
             //kasutada if ja else lauseid
-
-            {
-                Console.WriteLine("sisesta number");
-                string input = Console.ReadLine();
-                //kontrollime, kas sisestatud väärtus on number
-                if (int.TryParse(input, out int number))
-                {
-                    Console.WriteLine("sisestasid numbri");
-                }
-                else
-                {
                     Console.WriteLine("ERROR.Numbrid ei sisestatud");
-                }
 
 
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("sisesta number");
+            string input = Console.ReadLine();
+            //kontrollime, kas sisestatud väärtus on number
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"sisestasid numbri on:{number} ");
+            }
+            else
+            {
+                Console.WriteLine("Sisestatud väärtus ei ole number");
+            }
+        }
+    }
+}
