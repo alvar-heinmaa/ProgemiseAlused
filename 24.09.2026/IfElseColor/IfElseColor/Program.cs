67260db05cb073e391a771d558114f64b1071845
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 09:31:00 +0300

    Ifelsecolor.slnx

---
+++ 24.09.2026/IfElseColor/IfElseColor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace IfElseColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vali üks värv");
+            Console.WriteLine("valikus on: red,blue,green ja white");
+            //kirjuta konsooli tekst ja vajuta enter
+            string color = Console.ReadLine();
+            //see kontrollib,kas punane värv 
+            //kui kasutad if jaelse, siis esimene kontroll on if 
+            //ja järgmine else if ning kõige viimane on alati else
+            if (color == "blue") ;
+            {
+                Console.WriteLine("see on sinine");
+            }
+            else if (color == "red") ;
+            {
+                Console.WriteLine("see on punane");
+            }
+            else if (color == "green") ;
+        }
+             Console.WriteLine("see on roheline");
+    }
+    else if (color == "white");
+}
+Console.WriteLine("see on valge");
+    {
+    else 
+}
+Console.WriteLine("kahtlane värv");
+
+
+
+
+
