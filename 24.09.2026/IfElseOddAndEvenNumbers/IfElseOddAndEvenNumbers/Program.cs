78340700a58d283e26bf948f7835110381fceaa9
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 11:43:40 +0300

    IfElseOddAndEven

--- 24.09.2026/IfElseOddAndEvenNumbers/IfElseOddAndEvenNumbers/Program.cs
+++ 24.09.2026/IfElseOddAndEvenNumbers/IfElseOddAndEvenNumbers/Program.cs
@@ -9,15 +9,28 @@
 
             if (int.TryParse(sisend, out int nr))
             {
+                //% jagab muutjuat number 2
+                //kui 2 asemele panna 3, siis jagab 3-ga
                 if (nr % 2 == 0)
                 {
                     Console.WriteLine($"number {nr} on Paaris");
+                    //siia tuleb välja kutsuda meetod,
+                    //mis ütleb, et see on paarisarv
 
                 }
                 else
                 {
                     Console.WriteLine($"number {nr} on Paaritu");
+                    //siia tuleb välja kutsuda meetod,
+                    //mis ütleb, et see on paarituarv
                 }
+         static void EvenNumberMethod()
+
+                }
+                    {
+                    
+                }
+               
 
             } 
             //konsool küsib numbrit
